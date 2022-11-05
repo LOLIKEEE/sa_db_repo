4710ad8eebd2a3219c7274d3d7e2defa25c334e4
RaduOvidiu20 <99534761+RaduOvidiu20@users.noreply.github.com>
2022-11-05 20:37:25 +0200

    NumberOfDaysBetween homework has been uploaded

---
+++ radu_ovidiu/NumberOfDaysBetween/src/NumberOfDaysBetween.java
@@ -0,0 +1,22 @@
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
+public class NumberOfDaysBetween {
+
+
+    public static void main(String[] args) {
+        LocalDateTime newFirstDate = LocalDateTime.of(2021, Month.SEPTEMBER,1,10,10);
+        LocalDateTime newSecondDate = LocalDateTime.of(2022,Month.SEPTEMBER,22,22,22);
+
+        System.out.println("Number of days between first and second date is: " + getNumberOfDaysBetween(newFirstDate,newSecondDate));
+    }
+
+
+   public static int getNumberOfDaysBetween( LocalDateTime firstDate,LocalDateTime secondDate){
+        int daysBetween =(int) ChronoUnit.DAYS.between(firstDate,secondDate);
+        return daysBetween;
+
+    }
+}
